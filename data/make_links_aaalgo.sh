662b4563cac1502d16e442700c38a3feab3c1986
Wei Dong <wdong@wdong.org>
2018-06-19 12:40:06 -0400

    added physionet2018

--- data/make_links_aaalgo.sh
+++ data/make_links_aaalgo.sh
@@ -11,3 +11,6 @@ ln -si /shared/s2/data/benchmarks/bdd100k bdd100k
 ln -si /shared/s2/data/benchmarks/cityscape cityscape
 ln -si /shared/s2/data/benchmarks/coco coco
 ln -si /shared/s2/data/benchmarks/kitti kitti
+ln -si /shared/s2/data/benchmarks/kaggle_diabetic kaggle_diabetic
+ln -si /shared/s2/data/physionet/2018 physionet18
+
