1fd3b4e3645402c1d2ddc2c0f2fb5f432f2987e5
Wei Dong <wdong@wdong.org>
2018-06-26 12:21:41 -0400

    ADD

--- cityscape/eval_inst.py
+++ cityscape/eval_inst.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+import os
+import sys
+import subprocess as sp
+
+if len(sys.argv) != 2:
+    print("Usage: %s result_dir" % sys.argv[0])
+    sys.exit(0)
+
+result_dir = sys.argv[1]
+
+if not os.path.isdir(os.path.join(result_dir, 'data')):
+    print("Submission files should be in 'data' under result_dir.")
+    sys.exit(0)
+
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+eval_bin = os.path.abspath(os.path.join(ROOT, '../3rd/cityscapeScripts/cityscapescripts/evaluation/evalInstanceLevelSemanticLabeling.py'))
+
+gt_dir = os.path.abspath(os.path.join(ROOT, '../data/cityscape/'))
+
+
+sp.check_call('%s %s %s' % (eval_bin, gt_dir, result_dir), shell=True)
+
--- cityscape/eval_pixel.py
+++ cityscape/eval_pixel.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+import os
+import sys
+import subprocess as sp
+
+if len(sys.argv) != 2:
+    print("Usage: %s result_dir" % sys.argv[0])
+    sys.exit(0)
+
+result_dir = sys.argv[1]
+
+if not os.path.isdir(os.path.join(result_dir, "data")):
+    print("Submission files should be in 'data' under result_dir")
+    sys.exit(0)
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+eval_bin = os.path.abspath(os.path.join(ROOT, '../3rd/cityscapeScripts/cityscapescripts/evaluation/evalPixelLevelSemanticLabeling.py'))
+
+#gt_dir = os.path.abspath(os.path.join(ROOT, '../data/cityscape/gtFine/val/'))
+gt_dir = os.path.abspath(os.path.join(ROOT, 'CITYSCAPES_DATASET/gtFine/val/frankfurt'))
+
+sp.check_call('%s %s %s' % (eval_bin, gt_dir, results_dir), shell = True)
